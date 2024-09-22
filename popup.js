020e92c89a4639b514aed449ad4130893d6d25a4
andr3wtn <144362414+andr3wtn@users.noreply.github.com>
2024-09-22 12:43:07 -0500

    Update popup.js
    make stop button appear if background is called by already being at registration time

--- popup.js
+++ popup.js
@@ -174,6 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
               triggerBackgroundProcess();
               alert("It's registration time! Please proceed.");
               stopGame();
+              stopButton.classList.remove('hidden');
               return;
           }
 
